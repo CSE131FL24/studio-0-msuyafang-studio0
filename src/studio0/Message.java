def36170c95ffc3507095dd554423ba42c3ae27f
terryfxs <terryfxs@localhost>
2024-08-28 15:44:57 -0500

    I added a message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Evasion of responsibilities is fun");
 		System.out.println("Comparison is the biggest thief of joy");
+		System.out.println("Live a happy life");
 	}
 }
